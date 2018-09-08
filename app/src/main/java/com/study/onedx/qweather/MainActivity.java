76e6d8d08416631b41adf3ef79d2524aadf0c761
onedx <onedxu@gmail.com>
2018-09-08 22:21:58 +0800

    获取位置信息

--- app/src/main/java/com/study/onedx/qweather/MainActivity.java
+++ app/src/main/java/com/study/onedx/qweather/MainActivity.java
@@ -7,12 +7,18 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.study.onedx.qweather.util.HttpUtil;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -24,46 +30,51 @@ public class MainActivity extends AppCompatActivity {
     private final static int ACCESS_COARSE_LOCATION_REQUEST_CODE = 2;
     private final static int ACCESS_FINE_LOCATION_REQUEST_CODE = 3;
 
-    private final static String KEY = "qgxSWjkH1b03Ttdp";
+    private final static String CAIYUN_KEY = "qgxSWjkH1b03Ttdp";
 
     private TextView messageText;
 
+
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
     //经度
-    private String lon;
+    private String lon = null;
     //纬度
-    private String lat;
+    private String lat = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        messageText = (TextView) findViewById(R.id.message_text);
+        messageText = findViewById(R.id.message_text);
 
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(this,
-//                    new String[]{Manifest.permission.INTERNET},
-//                    INTERNET_REQUEST_CODE);
-//        }
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(this,
-//                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-//                    ACCESS_COARSE_LOCATION_REQUEST_CODE);
-//        }
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(this,
-//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-//                    ACCESS_FINE_LOCATION_REQUEST_CODE);
-//        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    INTERNET_REQUEST_CODE);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    ACCESS_COARSE_LOCATION_REQUEST_CODE);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    ACCESS_FINE_LOCATION_REQUEST_CODE);
+        }
 
 
         //获取当前地点
         getLocation();
-        //展示天气信息
-        getWeatherInfo();
+
     }
 
     @Override
@@ -80,19 +91,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getLocation(){
-        //纬度范围-90~90，经度范围-180~180
-        lat = "31.9701";
-        lon = "118.8110";
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //true表示使用定位缓存策略；false表示不使用。
+        mLocationOption.setLocationCacheEnable(false);
+        //true表示定位返回经纬度同时返回地址描述（定位类型是网络定位的会返回）；false表示不返回地址描述。
+        mLocationOption.setNeedAddress(false);
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，
+        // 启动定位时SDK会返回最近3s内精度最高的一次定位结果。
+        // 如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
     }
+
+    /**
+     * 声明定位回调监听器
+     */
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
+            if (aMapLocation != null) {
+                if (aMapLocation.getErrorCode() == 0) {
+                    //解析aMapLocation获取相应内容。
+                    //纬度范围-90~90，经度范围-180~180
+                    //aMapLocation.getLatitude();//获取纬度
+                    //aMapLocation.getLongitude();//获取经度
+                    //格式化参数：保留四位小数
+                    lat = new DecimalFormat("0.0000").format(aMapLocation.getLatitude());
+                    lon = new DecimalFormat("0.0000").format(aMapLocation.getLongitude());
+                }else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    Log.e("AmapError","location Error, ErrCode:"
+                            + aMapLocation.getErrorCode() + ", errInfo:"
+                            + aMapLocation.getErrorInfo());
+                }
+            }
+            //展示天气信息
+            getWeatherInfo();
+        }
+    };
 
     private void getWeatherInfo(){
         if(lat == null || lon == null){
             Toast.makeText(MainActivity.this, "can't get location", Toast.LENGTH_SHORT).show();
             return;
         }
-        String url = "https://api.caiyunapp.com/v2/" + KEY + "/" +
+        String url = "https://api.caiyunapp.com/v2/" + CAIYUN_KEY + "/" +
                 lon + "," + lat + "/realtime.json";
-        //"https://api.caiyunapp.com/v2/TAkhjf8d1nlSlspN/121.6544,25.1552/realtime.json"
         HttpUtil.sendOkHttpRequest(url, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -121,5 +176,7 @@ public class MainActivity extends AppCompatActivity {
     private void showWeatherInfo(String Weather){
 
     }
+
+
 
 }
