60eaedb984eba12b2d07f8218fdd55d4deed2455
onedx <onedxu@gmail.com>
2018-09-13 22:11:15 +0800

    布局调整

--- app/src/main/java/com/study/onedx/qweather/bean/HourForecast.java
+++ app/src/main/java/com/study/onedx/qweather/bean/HourForecast.java
@@ -2,4 +2,34 @@ package com.study.onedx.qweather.bean;
 
 public class HourForecast {
 
+    private String time;
+    private String skyInfo;
+    private String temperature;
+
+    public HourForecast() {
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getSkyInfo() {
+        return skyInfo;
+    }
+
+    public void setSkyInfo(String skyInfo) {
+        this.skyInfo = skyInfo;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
 }
